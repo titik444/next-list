4ff972c0c02d0e983596a040a10cd2ec76adad90
titik444 <momochi@yahoo.com>
2025-03-08 02:08:09 +0700

    feat: add Sidebar

--- client/src/app/darkboardWrapper.tsx
+++ client/src/app/darkboardWrapper.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
 
 export default function DashboardWrapper({
   children,
@@ -8,8 +9,7 @@ export default function DashboardWrapper({
 }) {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      {/* sidebar */}
-      sidebar
+      <Sidebar />
       <main
         className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
       >
