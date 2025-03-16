3c7de1a331876dea103ec1c72f6ffd0089f0254d
titik444 <momochi@yahoo.com>
2025-03-17 02:17:02 +0700

    feat: add ListView

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader";
 import Board from "../BoardView";
+import List from "../ListView";
 
 type Props = {
   params: { id: string };
@@ -17,6 +18,9 @@ export default function Project({ params }: Props) {
       <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === "Board" && (
         <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
     </div>
   );
