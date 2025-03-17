969c658ffe2ffef78143d7ca76bba51f8c12a417
titik444 <momochi@yahoo.com>
2025-03-18 01:27:11 +0700

    fix typo

--- client/src/app/components/TaskCard/index.tsx
+++ client/src/app/components/TaskCard/index.tsx
@@ -16,55 +16,51 @@ const TaskCard = ({ task }: Props) => {
           <div className="flex flex-wrap">
             {task.attachments && task.attachments.length > 0 && (
               <Image
-                src={`${task.attachments[0].fileUrl}`}
+                src={`/${task.attachments[0].fileURL}`}
                 alt={task.attachments[0].fileName}
                 width={400}
                 height={200}
                 className="rounded-md"
               />
             )}
-            <p>
-              <strong>ID:</strong> {task.id}
-            </p>
-            <p>
-              <strong>Title:</strong> {task.title}
-            </p>
-            <p>
-              <strong>Description:</strong>{" "}
-              {task.description || "No description provided"}
-            </p>
-            <p>
-              <strong>Status:</strong> {task.status}
-            </p>
-            <p>
-              <strong>Priority:</strong> {task.priority}
-            </p>
-            <p>
-              <strong>Tags:</strong> {task.tags || "No tags"}
-            </p>
-            <p>
-              <strong>Start Date:</strong>{" "}
-              {task.startDate
-                ? format(new Date(task.startDate), "P")
-                : "Not set"}
-            </p>
-            <p>
-              <strong>Start Date:</strong>{" "}
-              {task.startDate
-                ? format(new Date(task.startDate), "P")
-                : "Not set"}
-            </p>
-            <p>
-              <strong>Author:</strong>{" "}
-              {task.author ? task.author.username : "Unknown"}
-            </p>
-            <p>
-              <strong>Assignee:</strong>{" "}
-              {task.assignee ? task.assignee.username : "Unassigned"}
-            </p>
           </div>
         </div>
       )}
+      <p>
+        <strong>ID:</strong> {task.id}
+      </p>
+      <p>
+        <strong>Title:</strong> {task.title}
+      </p>
+      <p>
+        <strong>Description:</strong>{" "}
+        {task.description || "No description provided"}
+      </p>
+      <p>
+        <strong>Status:</strong> {task.status}
+      </p>
+      <p>
+        <strong>Priority:</strong> {task.priority}
+      </p>
+      <p>
+        <strong>Tags:</strong> {task.tags || "No tags"}
+      </p>
+      <p>
+        <strong>Start Date:</strong>{" "}
+        {task.startDate ? format(new Date(task.startDate), "P") : "Not set"}
+      </p>
+      <p>
+        <strong>Due Date:</strong>{" "}
+        {task.dueDate ? format(new Date(task.dueDate), "P") : "Not set"}
+      </p>
+      <p>
+        <strong>Author:</strong>{" "}
+        {task.author ? task.author.username : "Unknown"}
+      </p>
+      <p>
+        <strong>Assignee:</strong>{" "}
+        {task.assignee ? task.assignee.username : "Unassigned"}
+      </p>
     </div>
   );
 };
